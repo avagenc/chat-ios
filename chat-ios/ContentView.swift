fec29b3f33fc3847ee2ce4c154da4cb8460b48eb
Ardian <ardian@naturallyfunny.dev>
2026-07-09 13:32:07 +0700

    Initial Commit

---
+++ chat-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  chat-ios
+//
+//  Created by Ardian on 09/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ chat-ios/chat_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  chat_iosApp.swift
+//  chat-ios
+//
+//  Created by Ardian on 09/07/26.
+//
+
+import SwiftUI
+
+@main
+struct chat_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
